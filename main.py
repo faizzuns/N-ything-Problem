63f7ca7db7f1ba369d07406f499e1afddbabe478
yulyulyharuka <yulyharuka@gmail.com>
2018-09-22 18:40:06 +0700

    add read file

--- main.py
+++ main.py
@@ -1,37 +1,71 @@
+from os.path import exists
 from chess import *
 from board import Board
 
-def main():
-	c1 = Board()
-	point = c1.get_random_location()
-	queen_black = Queen(point['x'], point['y'], 'WHITE')
-	c1.add_piece(queen_black)
-	point = c1.get_random_location()
-	knight_black = Knight(point['x'], point['y'], 'WHITE')
-	c1.add_piece(knight_black)
-	point = c1.get_random_location()
-	bishop_black = Bishop(point['x'], point['y'], 'WHITE')
-	c1.add_piece(bishop_black)
-	point = c1.get_random_location()
-	rook_black = Rook(point['x'], point['y'], 'WHITE')
-	c1.add_piece(rook_black)
+def readFile(filename) :
+	if exists(filename) :
+		file = open(filename,"r")
+		row = 8
+		chess = []
+		for i in range(row) :
+			chess.append(file.readline().replace('\n',''))
 
-	point = c1.get_random_location()
-	queen_white = Queen(point['x'], point['y'], 'BLACK')
-	c1.add_piece(queen_white)
-	point = c1.get_random_location()
-	knight_white = Knight(point['x'], point['y'], 'BLACK')
-	c1.add_piece(knight_white)
-	point = c1.get_random_location()
-	bishop_white = Bishop(point['x'], point['y'], 'BLACK')
-	c1.add_piece(bishop_white)
-	point = c1.get_random_location()
-	rook_white = Rook(point['x'], point['y'], 'BLACK')
-	c1.add_piece(rook_white)
+		bidak = []
+		for x in chess :
+			bidak.append(x.split(' '))
 
-	c1.print_board()
-	c1.print_piece()
-	c1.print_info()
+		print (bidak)
+		return bidak
+	else :
+		print ("File not exist.")
 
+def intiateBoard() :
+	list_bidak = []
+	list_bidak = readFile("catur.txt")
+	chess_board = Board()
+	for x in list_bidak :
+		point = chess_board.get_random_location()
+		if (x[1] == 'KNIGHT') :
+			for i in range(int(x[2])) :
+				point = chess_board.get_random_location()
+				if (x[0] == 'WHITE') :
+					white_knight = Knight(point['x'],point['y'],'WHITE')
+					chess_board.add_piece(white_knight)
+				else :
+					black_knight = Knight(point['x'],point['y'],'BLACK')
+					chess_board.add_piece(black_knight)
+		elif (x[1] == 'BISHOP') :
+			for i in range(int(x[2])) :
+				point = chess_board.get_random_location()
+				if (x[0] == 'WHITE') :
+					white_bishop = Bishop(point['x'],point['y'],'WHITE')
+					chess_board.add_piece(white_bishop)
+				else:
+					black_bishop = Bishop(point['x'],point['y'],'BLACK')
+					chess_board.add_piece(black_bishop)
+		elif (x[1] == 'QUEEN') :
+			for i in range(int(x[2])) :
+				point = chess_board.get_random_location()
+				if (x[0] == 'WHITE') :
+					white_queen = Queen(point['x'],point['y'],'WHITE')
+					chess_board.add_piece(white_queen)
+				else :
+					black_queen = Queen(point['x'],point['y'],'BLACK')
+					chess_board.add_piece(black_queen)
+		elif (x[1] == 'ROOK') :
+			for i in range(int(x[2])) :
+				point = chess_board.get_random_location()
+				if (x[0] == 'WHITE') :
+					white_rook = Rook(point['x'],point['y'],'WHITE')
+					chess_board.add_piece(white_rook)
+				else : 
+					black_rook = Rook(point['x'],point['y'],'BLACK')
+					chess_board.add_piece(black_rook)
 
-main()
+	return chess_board
+
+chess = intiateBoard()
+chess.print_board()
+chess.print_piece()
+chess.print_info()
+# main()
